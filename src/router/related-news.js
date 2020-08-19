a86e6c5dc0c431b6c7bcc99e2726846ee6e1d696
mi-goto <mi-goto@b-tm.co.jp>
2020-08-19 19:27:27 +0900

    関連記事API修正

--- src/router/related-news.js
+++ src/router/related-news.js
@@ -2,11 +2,14 @@ const router = require('express').Router();
 const db = require('../db/db-config');
 
 const models = require('../models/');
-const News = models.news;
 
 router.get('/', (req, res) => {
   db.transaction(async t => {
-    const news = await News.findAll({ order: [['id', 'ASC']] });
+    const news = await models.news.findAll({
+      order: [['id', 'ASC']],
+      limit: 4,
+      include: models.category
+    });
     return Promise.resolve(res.send(news));
   });
 });
